bf747799cfe81cfc53254e8f28b27d2c1ce4fbfb
simar <simararneja@in.ibm.com>
2018-11-01 17:04:03 +0530

    reader writer interface

--- 07_http/main.go
+++ 07_http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,5 +16,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// Read function doesn't resize the byte slice
+	/*
+		resp.Body.Read(bs)
+		fmt.Println(string(bs))
+		bs := make([]byte, 99999)
+	*/
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Wrote this many bytes", len(bs))
+	return len(bs), nil
 }
